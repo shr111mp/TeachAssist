2f4989218f3a5d83b8edf2511e7ced8e4ed3804e
shr111mp <castillobryan.bc67@gmail.com>
2024-08-30 17:12:40 +0800

    FIRST COMMIT

---
+++ TeachAssist2/app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    //Added the google services plugin
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.teachassist"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.teachassist"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.play.services.base)
+    implementation(libs.firebase.auth)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    //Firebase BOM
+    implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
+    implementation("com.google.firebase:firebase-analytics")
+}
---
+++ TeachAssist2/app/src/main/java/com/example/teachassist/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.example.teachassist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.common.SignInButton;
+
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        TextView textSignUp = findViewById(R.id.textViewSignUp);
+
+        textSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
+
+
+
+
+
+    }
+
+
+}
